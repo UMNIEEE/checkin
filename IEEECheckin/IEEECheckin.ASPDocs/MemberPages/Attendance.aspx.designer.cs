6b69cd04daf922f71755d31d454e605017b82582
Bryan Stadick <stadi012@umn.edu>
2015-05-21 22:39:43 -0500

    Added lottery roll function to attendance page.

--- IEEECheckin/IEEECheckin.ASPDocs/MemberPages/Attendance.aspx.designer.cs
+++ IEEECheckin/IEEECheckin.ASPDocs/MemberPages/Attendance.aspx.designer.cs
@@ -74,5 +74,14 @@ namespace IEEECheckin.ASPDocs.MemberPages {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button ClearButton;
+        
+        /// <summary>
+        /// RandomButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button RandomButton;
     }
 }
